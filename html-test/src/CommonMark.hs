1e95a641d07f4798a54c34646e2b84c974eb092c
John MacFarlane <jgm@berkeley.edu>
2015-04-11 15:32:14 -0700

    Improved CommonMark.hs test case.

--- html-test/src/CommonMark.hs
+++ html-test/src/CommonMark.hs
@@ -1,3 +1,57 @@
+{-|md
+CommonMark in Haddock
+---------------------
+
+This module demonstrates CommonMark comment syntax.
+
+1. A list
+   - an unordered sublist
+
+     with a second paragraph
+   - another item in the same sublist
+
+2. Another list item
+
+``` haskell
+this `is` a "code block" >>= putStrLn
+```
+
+    So is this.
+
+You can have code examples as usual in haddock. These are
+automatically recognized if they begin with `>>> `.
+
+```
+>>> fib 5
+5
+>>> fib 10
+55
+```
+
+    >>> fib 10
+    55
+
+Example with an import:
+
+    >>> import Data.Char
+    >>> isSpace 'a'
+    False
+
+    >>> putStrLn "foo\n\nbar"
+    foo
+
+    bar
+
+Here's a [link to the CommonMark spec](http://spec.commonmark.org)
+and an autolink: <http://spec.commonmark.org>.
+
+----
+
+Raw <a href="foobar">HTML</a> and horizontal rules are just ignored,
+as they have no haddock equivalent.
+
+> Block quotes are treated as regular paragraphs.
+-}
 module CommonMark where
 
 data A = A
@@ -6,13 +60,15 @@ other :: Int
 other = 2
 
 -- |md
--- Markdown *Doc* for [link](url) `test2`
+-- Here is a CommonMark description of `test2`.
+-- A code example:
+--
+--    test2 == False
 test2 :: Bool
 test2 = False
 
--- | Not *markdown*
+-- | This comment does not begin with `md`, so it is processed
+-- as regular /haddock markup/, not *CommonMark*.  So we can
+-- talk about 'X', for example, and this will be made into
+-- a link.
 data X = X -- ^ Doc for consructor
-
--- | Should show up on the page for both modules A and B
-reExport :: Int
-reExport = 1
